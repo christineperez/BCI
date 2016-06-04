5aa5407c86ab3fda9f7e2651c9930f23359d554d
gracielaq <gqstar30@gmail.com>
2016-06-05 02:03:10 +0800

    FollowUpDisease Status Bean

---
+++ src/Bio/model/FollowUpDiseaseStatus.java
@@ -0,0 +1,34 @@
+package Bio.model;
+
+public class FollowUpDiseaseStatus {
+private int followUpDiseaseStatusId;
+private String diseaseStatus;
+private String others;
+private String notes;
+public int getFollowUpDiseaseStatusId() {
+	return followUpDiseaseStatusId;
+}
+public void setFollowUpDiseaseStatusId(int followUpDiseaseStatusId) {
+	this.followUpDiseaseStatusId = followUpDiseaseStatusId;
+}
+public String getDiseaseStatus() {
+	return diseaseStatus;
+}
+public void setDiseaseStatus(String diseaseStatus) {
+	this.diseaseStatus = diseaseStatus;
+}
+public String getOthers() {
+	return others;
+}
+public void setOthers(String others) {
+	this.others = others;
+}
+public String getNotes() {
+	return notes;
+}
+public void setNotes(String notes) {
+	this.notes = notes;
+}
+
+
+}
