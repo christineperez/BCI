89d4bdfb8c55efd47e8e26521fe40334bdf3e811
gracielaq <gracielaquiambao@GQs-Macbook-Air.local>
2016-09-02 22:49:34 +0800

    Added AddPatientBean, updated BioBeanFactory and SQLCommands for Add Patient

---
+++ src/Bio/model/AddPatientBean.java
@@ -0,0 +1,79 @@
+package Bio.model;
+
+import java.sql.Date;
+
+public class AddPatientBean {
+	private int patientId;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String gender;
+	private Date date;
+	private String address;
+	private String civilStatus;
+	private String occupation;
+	private Date dateOfEntry;
+	
+	public int getPatientId() {
+		return patientId;
+	}
+	public void setPatientId(int patientId) {
+		this.patientId = patientId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCivilStatus() {
+		return civilStatus;
+	}
+	public void setCivilStatus(String civilStatus) {
+		this.civilStatus = civilStatus;
+	}
+	public String getOccupation() {
+		return occupation;
+	}
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+	public Date getDateOfEntry() {
+		return dateOfEntry;
+	}
+	public void setDateOfEntry(Date dateOfEntry) {
+		this.dateOfEntry = dateOfEntry;
+	}
+	
+	
+}
--- src/Bio/utility/BioBeanFactory.java
+++ src/Bio/utility/BioBeanFactory.java
@@ -1,9 +1,12 @@
 package Bio.utility;
+import java.sql.Date;
+
 import Bio.model.*;
 
 public class BioBeanFactory {
 	public static AccountBean getFactoryBean(int accountID,int personalInfoID, String username, String password, String salt, int roleID, int active) {
 		AccountBean ab = new AccountBean();
+		
 		ab.setAccountID(accountID);
 		ab.setActive(active);
 		ab.setPassword(password);
@@ -13,5 +16,23 @@ public class BioBeanFactory {
 		ab.setUsername(username);
 		
 		return ab;
+	}
+	
+	public static AddPatientBean getFactoryBean(int patientId, String firstName, String middleName, String lastName, String gender, Date date, String address, String civilStatus,
+			String occupation, Date dateOfEntry){
+		AddPatientBean addPatientBean= new AddPatientBean();
+		
+		addPatientBean.setPatientId(patientId);
+		addPatientBean.setFirstName(firstName);
+		addPatientBean.setMiddleName(middleName);
+		addPatientBean.setLastName(lastName);
+		addPatientBean.setGender(gender);
+		addPatientBean.setDate(date);
+		addPatientBean.setAddress(address);
+		addPatientBean.setCivilStatus(civilStatus);
+		addPatientBean.setOccupation(occupation);
+		addPatientBean.setDateOfEntry(dateOfEntry);
+		
+		return addPatientBean;
 	}
 }
